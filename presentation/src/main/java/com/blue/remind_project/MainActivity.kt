7220de9b61fdd27e662db9956d5c05bfc2351370
pureum <lpm0831@naver.com>
2024-07-20 23:55:20 +0900

    SavedStateHandle

--- presentation/src/main/java/com/blue/remind_project/MainActivity.kt
+++ presentation/src/main/java/com/blue/remind_project/MainActivity.kt
@@ -1,16 +1,49 @@
 package com.blue.remind_project
 
+import android.app.Application.ActivityLifecycleCallbacks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.blue.remind_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel: MainViewModel by viewModels()
+//    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by lazy {
+    ViewModelProvider(this)[MainViewModel::class.java]
+}
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding.test.text = "Pureum"
+        viewModel.filteredData.observe(this){
+            binding.test.text = it ?: "값 비었어용"
+        }
+        binding.button.setOnClickListener {
+            viewModel.setQuery()
+        }
         setContentView(binding.root)
+    }
+
+
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
--- presentation/src/main/java/com/blue/remind_project/ViewModel.kt
+++ presentation/src/main/java/com/blue/remind_project/ViewModel.kt
@@ -2,11 +2,18 @@ package com.blue.remind_project
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 
-class MainViewModel: ViewModel(){
-//    private var _test = MutableLiveData<Int>()
-//    val test: LiveData<Int> = _test
+class MainViewModel(private val savedStateHandle: SavedStateHandle): ViewModel(){
+    private var _test = MutableLiveData<Int>()
+    val test: LiveData<Int> = _test
 
+    val filteredData:MutableLiveData<String> =
+        savedStateHandle.getLiveData("query")
 
+    fun setQuery(){
+        savedStateHandle["query"] = "탈모쥐"
+    }
 }
